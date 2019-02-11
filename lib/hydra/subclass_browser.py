cedc4a075c1c3b717e4a1d7ed1212b656aa5b5ff
Jesse Weinstein <jesse@wefu.org>
2019-02-10 18:49:44 -0800

    It is usable!

--- lib/hydra/subclass_browser.py
+++ lib/hydra/subclass_browser.py
@@ -16,23 +16,30 @@ class Entry:
         self.current_idx = -1
         self.all_loaded = False
 
-    def make_window(self, y, x):
+    def load(self, y, x):
         self.y = y
         self.x = x
-        self.window = curses.newwin(3, 2, self.y, self.x)
         self.adjust_current(+1)
         return self
 
     @property    
     def label(self):
         maybe_label = g.preferredLabel(self.wd, 'en', labelProperties=(RDFS.label,))
-        self.label = str(maybe_label[0][1]) if maybe_label else self.code
+        self.label = maybe_label[0][1].encode('utf-8') if maybe_label else self.code
         return self.label
+
+    @property
+    def window(self):
+        self.window = curses.newwin(3, 2, self.y, self.x)
+        return self.window
     
     def open_current(self):
-        return self.loaded_subclasses[self.current_idx].make_window(
-            self.y + self.current_idx,
-            self.x + self.window.getmaxyx()[1] + 2)
+        new = self.loaded_subclasses[self.current_idx].load(
+            self.y + self.current_idx+1,
+            self.x + self.window.getmaxyx()[1])
+        if not new.loaded_subclasses:
+            return self
+        return new
 
     def adjust_current(self, direction):
         self.current_idx += direction
@@ -47,23 +54,35 @@ class Entry:
                 except StopIteration:
                     self.all_loaded = True
                     self.current_idx = len(self.loaded_subclasses)-1
-        self.redisplay()
+        if self.loaded_subclasses:
+            self.redisplay()
+        else:
+            self.window.resize(1,2)
+            self.window.clear()
+            self.window.addstr(0,0,'*')
+            self.window.refresh()
 
     def redisplay(self):
-        if len(self.loaded_subclasses):
-            self.window.resize(len(self.loaded_subclasses) + 2, max(
-                len(self.loaded_subclasses[-1].label)+2,
-                self.window.getmaxyx()[1]))
+        ci = str(self.current_idx)
+        self.window.resize(len(self.loaded_subclasses) + 2, max(
+            len(ci) + len(self.code) + 3,
+            (len(self.loaded_subclasses[-1].label) + 2) if len(self.loaded_subclasses) else 0,
+            self.window.getmaxyx()[1]))
         self.window.clear()
         self.window.border()
-        self.window.addstr(0,0,str(self.current_idx), curses.A_BOLD if self.all_loaded else curses.A_NORMAL)
+        self.window.addstr(0,0, ci, curses.A_BOLD if self.all_loaded else curses.A_NORMAL)
+        self.window.addstr(0, len(ci) + 1, self.code)
         for idx, e in enumerate(self.loaded_subclasses):
             self.window.addstr(idx+1, 1, e.label, curses.A_BOLD if idx == self.current_idx else curses.A_NORMAL)
         self.window.refresh()
 
 def main(s):
     global WD, WDT
-    curses.curs_set(0)
+    try:
+        s.leaveok(0)
+        curses.curs_set(0)
+    except curses.error:
+        pass
     s.clear()
     s.border()
     s.addstr("Wikidata Subclasses", curses.A_BOLD)
@@ -74,8 +93,9 @@ def main(s):
     WDT=Namespace(g.store.namespace('wdt'))
 
     active = Entry('Q35120')
-    active.make_window(1,1)
+    active.load(1,1)
     while 1:
+        s.move(0,0)
         c = s.getch()
         if c == curses.KEY_UP:
             active.adjust_current(-1)
@@ -84,29 +104,9 @@ def main(s):
         elif c == curses.KEY_RIGHT:
             active = active.open_current()
         elif c == curses.KEY_LEFT and active.parent:
-            active = active.parent 
+            active = active.parent
         elif c == ord('q'):
             break
-
-def main2(s):
-    s.clear()
-    s.border()
-    s.addstr("what")
-    s.refresh()
-
-    s2=curses.newwin(3, 30, 1,1)
-    s2.clear()
-    while 1:
-        s2.border()
-        s2.addstr(1,1, "***")
-        s2.refresh()
-        c = s.getch()
-        if c == ord('q'):
-            break
-        elif c == curses.KEY_DOWN:
-            y, x = s2.getmaxyx()
-            s2.resize(y + 1, x)
-            s2.clear()
 
 if __name__ == '__main__':
     curses.wrapper(main)
